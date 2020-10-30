0948450e62da2f7e3a2b8a07927d65a9aca88394
Mattias Leth <Sjunneson@hotmail.com>
2020-10-30 13:16:41 +0100

    asd

--- mainKlasse/src/Person.java
+++ mainKlasse/src/Person.java
@@ -2,4 +2,10 @@ public class Person
 {
   private String name;
   private String addresse;
+
+  public Person(String name, String addresse)
+  {
+    this.name = name;
+    this.addresse = addresse;
+  }
 }
